ef352734ad8c6ab09bee070b897249be51370b5d
MasedMSD <masedmsd@mail.ru>
2024-04-01 09:09:19 +0500

    github format actions changes

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { config } from "dotenv";
 import { resolve } from "path";
 import { Client } from "./structures";
 
-config({ path: resolve(process.cwd(), ".env") });
+config({path: resolve(process.cwd(), ".env") });
 const client = new Client();
 
 client.init();
