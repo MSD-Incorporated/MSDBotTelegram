237d13774694b8bb2fb8d5f9e446ac4493c492cc
MasedMSD <roblox.hack@mail.ru>
2024-04-01 17:53:35 +0500

    eval command

--- src/elements/commands/index.ts
+++ src/elements/commands/index.ts
@@ -1,2 +1,5 @@
+export * from "./developer";
+
 export * from "./start";
 export * from "./telegraph";
+
--- src/structures/client.ts
+++ src/structures/client.ts
@@ -1,5 +1,5 @@
 import { Bot, type Context } from "grammy";
-import { autoLink, autoLinkEdited, startCommand, telegraphCommand } from "../elements";
+import { autoLink, autoLinkEdited, evalCommand, startCommand, telegraphCommand } from "../elements";
 
 export class Client {
 	public readonly bot: Bot<Context>;
@@ -14,8 +14,18 @@ export class Client {
 
 		this.bot.command("start", startCommand);
 		this.bot.command("telegraph", telegraphCommand);
+		this.bot.command("eval", (ctx) => evalCommand(ctx, this));
+
 		this.bot.on("channel_post", autoLink);
 		this.bot.on("edited_channel_post", autoLinkEdited);
+
+		this.bot.catch((err) => {
+			console.error(err);
+
+			err.ctx.reply("Произошла неизвестная ошибка!", {
+				reply_parameters: { message_id: err.ctx.message!.message_id },
+			});
+		});
 
 		// TODO: Setup https://github.com/grammyjs/runner
 		await this.bot.start({
