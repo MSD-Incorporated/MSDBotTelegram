07d248c2178260266daf3a0ae14314c14c2094e4
MasedMSD <roblox.hack@mail.ru>
2024-05-19 22:57:31 +0500

    banned users

--- src/elements/commands/developer/gemini.ts
+++ src/elements/commands/developer/gemini.ts
@@ -9,8 +9,9 @@ console.log(process.env);
 
 const genAI = new GoogleGenerativeAI(process.env.GOOGLE_AI_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-pro" });
-const allowedUsers = [946070039, 629401289, 654382771, 954735954];
-const allowedChats = [-1001705068191, -1001860827131];
+const allowedUsers: number[] = [946070039, 629401289, 654382771, 954735954];
+const bannedUsers: number[] = [823839281];
+const allowedChats: number[] = [-1001705068191, -1001860827131];
 
 const parser = (str: string) => {
 	if (str.match(/^```(.*)\n([\s\S]*?)```$/gm)) {
@@ -29,6 +30,8 @@ const parser = (str: string) => {
 };
 
 export const geminiCommand = async (ctx: Context) => {
+	if (bannedUsers.includes(ctx.from!.id)) return;
+
 	if (!allowedChats.includes(ctx.chat!.id)) {
 		if (!allowedUsers.includes(ctx.from!.id)) return;
 	}
