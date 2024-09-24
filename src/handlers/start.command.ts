b6ac4dea4f196677c7ac88c8a65c4575b5d35d7a
MasedMSD <masedmsd@mail.ru>
2024-09-24 14:04:46 +0500

    little fixes

--- src/handlers/start.command.ts
+++ src/handlers/start.command.ts
@@ -2,8 +2,8 @@ import { InlineKeyboard, type Context } from "grammy";
 
 const version = process.env.npm_package_version;
 
-export const startCommand = async ({ reply }: Context) => {
-	return reply(["Добро пожаловать!", `Текущий билд: <code>${version}</code>`].join("\n\n"), {
+export const startCommand = async (ctx: Context) => {
+	return ctx.reply(["Добро пожаловать!", `Текущий билд: <code>${version}</code>`].join("\n\n"), {
 		parse_mode: "HTML",
 		reply_markup: new InlineKeyboard().add({
 			text: "🔗 • Github",
