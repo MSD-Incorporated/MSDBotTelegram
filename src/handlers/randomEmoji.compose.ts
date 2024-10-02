7acbd16aa634a4c3a26dc2c3128853fc6890c032
MasedMSD <masedmsd@mail.ru>
2024-10-02 17:54:24 +0500

    added return

--- src/handlers/randomEmoji.compose.ts
+++ src/handlers/randomEmoji.compose.ts
@@ -4,6 +4,5 @@ export const randomEmojiComposer = new Composer();
 
 randomEmojiComposer.on("message:text", async ctx => {
 	if (ctx.message.from.is_bot) return;
-
-	if (Math.random() < 0.09) ctx.react("👀");
+	if (Math.random() < 0.09) return ctx.react("👀");
 });
