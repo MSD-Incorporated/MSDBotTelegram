a3308162295f48c2e0179f696220b75040703d0c
MasedMSD <masedmsd@mail.ru>
2024-10-02 17:54:13 +0500

    fixed bot

--- src/handlers/randomEmoji.compose.ts
+++ src/handlers/randomEmoji.compose.ts
@@ -3,5 +3,7 @@ import { Composer } from "grammy";
 export const randomEmojiComposer = new Composer();
 
 randomEmojiComposer.on("message:text", async ctx => {
+	if (ctx.message.from.is_bot) return;
+
 	if (Math.random() < 0.09) ctx.react("👀");
 });
