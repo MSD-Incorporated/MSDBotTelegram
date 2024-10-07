2b5350294ac457fbc30fa07573b81e829f228cb9
MasedMSD <masedmsd@mail.ru>
2024-10-07 19:36:50 +0500

    removed privacy

--- src/handlers/privacy.command.ts
+++
@@ -1,9 +0,0 @@
-import type { Context } from "grammy";
-
-const privacyLink = "https://telegra.ph/Politika-konfidencialnosti-dlya-telegram-bota-masedmsd-bot-07-05";
-
-export const privacyCommand = async (ctx: Context) => {
-	return ctx.reply(`<a href="${privacyLink}">Политика конфиденциальности</a>`, {
-		parse_mode: "HTML",
-	});
-};
