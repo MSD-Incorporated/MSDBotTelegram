b7948e511ea2be8ba22c9a7e4f70244e397bd74b
MasedMSD <68379695+MasedMSD@users.noreply.github.com>
2024-12-02 21:11:37 +0500

    textSanitazer for formatter

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,1 @@
 import { Bot } from "grammy";
-import { Formatter } from "./utils/formatter";
-
-const client = new Bot(process.env.TOKEN);
-const formatter = new Formatter("HTML");
-
-client.api.sendMessage(946070039, formatter.pre("console.log('Hello world')", "typescript"), {
-	parse_mode: "HTML",
-});
--- src/utils/formatter.ts
+++ src/utils/formatter.ts
@@ -1,5 +1,13 @@
 import { ParseMode } from "typegram";
 
+export const textSanitaizer = (text: string) =>
+	text
+		.replace(/&/g, "&amp;")
+		.replace(/</g, "&lt;")
+		.replace(/>/g, "&gt;")
+		.replace(/"/g, "&quot;")
+		.replace(/'/g, "&#039;");
+
 export class Formatter {
 	readonly type: ParseMode;
 
@@ -8,16 +16,16 @@ export class Formatter {
 	}
 
 	readonly blockqoute = (content: string): string => {
-		return `<blockquote class="tg-blockquote">${content}</blockquote>`;
+		return `<blockquote class="tg-blockquote">${textSanitaizer(content)}</blockquote>`;
 	};
 
 	readonly bold = (content: string): string => {
-		return `<b class="tg-bold">${content}</b>`;
+		return `<b class="tg-bold">${textSanitaizer(content)}</b>`;
 	};
 
 	readonly pre = (content: string, language?: string): string => {
 		return language === undefined
-			? `<pre class="tg-pre-code"><code>${content}</code></pre>`
-			: `<pre class="tg-pre-code"><code class="language-${language}">${content}</code></pre>`;
+			? `<pre class="tg-pre-code"><code>${textSanitaizer(content)}</code></pre>`
+			: `<pre class="tg-pre-code"><code class="language-${language}">${textSanitaizer(content)}</code></pre>`;
 	};
 }
