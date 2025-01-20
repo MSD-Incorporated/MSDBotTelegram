c19bc714eab527ba01bfd82c7a5c3de386ab1c50
MasedMSD <68379695+MasedMSD@users.noreply.github.com>
2025-01-20 16:29:15 +0500

    msdincorporated composer & shitposts

---
+++ src/elements/msdIncorporated.ts
@@ -0,0 +1,104 @@
+import { Composer, InputFile } from "grammy";
+import { resolve } from "path";
+import { cwd } from "process";
+import sagiri from "sagiri";
+import type { Message, MessageEntity } from "typegram";
+import type { Context } from "../utils";
+
+const channelID = -1001528929804;
+const chatChannelID = -1001705068191;
+
+const urlParser = (urls: string[]) => {
+	const sortedURLs: [string, string][] = [];
+
+	urls.forEach(url => {
+		if (url.includes("gelbooru")) sortedURLs.push(["Gelbooru", url]);
+		if (url.includes("danbooru")) sortedURLs.push(["Danbooru", url]);
+		if (url.includes("x.com") || url.includes("twitter"))
+			sortedURLs.push(["Twitter", url.replace("twitter.com", "fxtwitter.com").replace("x.com", "fxtwitter.com")]);
+		if (url.includes("pixiv")) sortedURLs.push(["Pixiv", url]);
+		if (url.includes("yande.re")) sortedURLs.push(["Yandere", url]);
+		if (url.includes("patreon")) sortedURLs.push(["Patreon", url]);
+	});
+
+	return sortedURLs;
+};
+
+export const msdIncorporatedComposer = new Composer<Context>();
+
+msdIncorporatedComposer.on(["channel_post::hashtag", "edited_channel_post::hashtag"], async ctx => {
+	const post = ctx.channelPost || ctx.editedChannelPost;
+	if (channelID !== post.chat.id) return;
+
+	const entities = (post.caption ? post.caption_entities : post.entities)!;
+	if (entities.find(entity => entity.type === "text_link" && entity.url === "https://t.me/msd_inc")) return;
+
+	const chatID = post.chat.id;
+	const message_id = post.message_id;
+	const original = post.caption ? post.caption : post.text;
+	const str = original + "\n\n" + "MSD Incorporated" + " | Donate";
+
+	const linkEntity: MessageEntity = {
+		length: "MSD Incorporated".length,
+		offset: str.length - " | Donate".length - "MSD Incorporated".length,
+		type: "text_link",
+		url: "https://t.me/msd_inc",
+	};
+
+	const donateEntity: MessageEntity = {
+		length: "Donate".length,
+		offset: str.length - "Donate".length,
+		type: "text_link",
+		url: "https://t.me/msd_inc/14",
+	};
+
+	return post.caption
+		? ctx.api.editMessageCaption(chatID, message_id, {
+				caption: str,
+				caption_entities: [...entities, linkEntity, donateEntity],
+			})
+		: ctx.api.editMessageText(chatID, message_id, str, {
+				entities: [...entities, linkEntity, donateEntity],
+			});
+});
+
+msdIncorporatedComposer.on(":photo").on(":is_automatic_forward", async ctx => {
+	if (channelID !== (ctx.message?.forward_origin! as Message).chat.id) return;
+	if (!ctx.message?.caption?.includes("#Hentai")) return;
+	if (ctx.message?.caption?.includes("#RealLife") || ctx.message?.caption?.includes("#Video")) return;
+	if (ctx.message?.media_group_id) return;
+
+	const file = await ctx.getFile();
+	const url = `https://api.telegram.org/file/bot${process.env.TOKEN}/${file.file_path!}`;
+
+	const sauceNao = sagiri(process.env.SAUCENAO_TOKEN);
+	const [res] = await sauceNao(url);
+
+	if (!res?.raw.data.ext_urls.length) return;
+
+	const { author, creator, characters } = res.raw.data;
+	// @ts-ignore
+	const material = res.raw.data.material;
+	const urls = [...res.raw.data.ext_urls, res.raw.data.source].filter(val => val !== undefined);
+
+	if (!url.length) return;
+
+	return ctx.reply(
+		[
+			`• <b>Автор:</b> <code>${author || creator || "Неизвестно"}</code>`,
+			`• <b>Персонажи:</b> <code>${(characters || "Неизвестно").split(", ").join("</code>, <code>") || "Неизвестно"}</code>`,
+			`• <b>Откуда:</b> <code>${material || "Неизвестно"}</code>\n`,
+			`• <b>Ссылки:</b> ${urlParser(urls)
+				.map(([name, url]) => `<a href="${url}">${name}</a>`)
+				.join(" | ")}`,
+		].join("\n"),
+		{
+			parse_mode: "HTML",
+		}
+	);
+});
+
+msdIncorporatedComposer.on(":new_chat_members", async ctx => {
+	if (ctx.chat.id !== chatChannelID) return;
+	ctx.replyWithAnimation(new InputFile(resolve(cwd(), "src", "media", "welcome.gif")));
+});
---
+++ src/elements/shitposts.ts
@@ -0,0 +1,32 @@
+import { checkText } from "@artemis69/yandex-speller";
+import { Composer } from "grammy";
+import type { Message } from "typegram";
+import type { Context } from "../utils";
+
+export const shitpostsComposer = new Composer<Context>();
+
+const channelID = -1002118873453;
+const safeWord = (word: string) => word.replace(/</g, "&lt;").replace(/>/g, "&gt;");
+
+shitpostsComposer.on("message", async (ctx, next) => {
+	if (!ctx.message.forward_origin || (ctx.message?.forward_origin! as Message).chat.id !== channelID) return next();
+
+	const text = ctx.message.text || ctx.message.caption;
+	if (!text) return next();
+
+	const words = await checkText(text, {});
+	if (!words.length) return next();
+
+	const rows = text.split("\n");
+	const formattedRows = words.map(({ word, s, row }) => {
+		const correctedRow = rows[row]?.replace(new RegExp(`${safeWord(word)}\\s`, "gm"), `${safeWord(s[0]!)} `);
+
+		return `Слово <code>${safeWord(word)}</code> должно быть исправлено на <code>${safeWord(s[0]!)}
+			\n<pre class="tg-pre-code"><code class="language-diff">- ${rows[row]}
+			\n+ ${correctedRow}</code></pre>`;
+	});
+
+	const textToSend = formattedRows.join("\n\n");
+
+	return ctx.reply(`<b>Я нашёл возможные ошибки в тексте:</b>\n\n${textToSend}`, { parse_mode: "HTML" });
+});
