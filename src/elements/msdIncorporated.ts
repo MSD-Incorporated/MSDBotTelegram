6990fabc3d882249277fd7bf70b12a5241793998
MasedMSD <68379695+MasedMSD@users.noreply.github.com>
2025-01-20 16:31:14 +0500

    removed link adder

--- src/elements/msdIncorporated.ts
+++ src/elements/msdIncorporated.ts
@@ -26,35 +26,6 @@ const urlParser = (urls: string[]) => {
 
 export const msdIncorporatedComposer = new Composer<Context>();
 
-msdIncorporatedComposer.on(["channel_post::hashtag", "edited_channel_post::hashtag"], async ctx => {
-	const post = ctx.channelPost || ctx.editedChannelPost;
-	if (channelID !== post.chat.id) return;
-
-	const entities = (post.caption ? post.caption_entities : post.entities)!;
-	if (entities.find(entity => entity.type === "text_link" && entity.url === "https://t.me/msd_inc")) return;
-
-	const chatID = post.chat.id;
-	const message_id = post.message_id;
-	const original = post.caption ? post.caption : post.text;
-	const str = original + "\n\n" + "MSD Incorporated";
-
-	const linkEntity: MessageEntity = {
-		length: "MSD Incorporated".length,
-		offset: str.length - "MSD Incorporated".length,
-		type: "text_link",
-		url: "https://t.me/msd_inc",
-	};
-
-	return post.caption
-		? ctx.api.editMessageCaption(chatID, message_id, {
-				caption: str,
-				caption_entities: [...entities, linkEntity],
-			})
-		: ctx.api.editMessageText(chatID, message_id, str, {
-				entities: [...entities, linkEntity],
-			});
-});
-
 msdIncorporatedComposer.on(":photo").on(":is_automatic_forward", async ctx => {
 	if (channelID !== (ctx.message?.forward_origin! as Message).chat.id) return;
 	if (!ctx.message?.caption?.includes("#Hentai")) return;
