df8d2e486cae90a00ef0f2d37054ada8636259e2
MasedMSD <68379695+MasedMSD@users.noreply.github.com>
2025-04-27 15:08:36 +0500

    caching

---
+++ src/utils/caching.ts
@@ -0,0 +1,35 @@
+import type { NextFunction } from "grammy";
+
+import type { Database } from "../structures/database";
+import type { Context } from "./context";
+
+export const autoUserCaching = async (
+	ctx: Context & { database: Database },
+	database: Database,
+	next: NextFunction
+) => {
+	ctx.database = database;
+
+	const user = ctx.from;
+	if (!user || user.is_bot || user.id == 777000) return next();
+
+	const dbuser = await database.resolveUser(user, true);
+
+	if (
+		user.first_name !== dbuser.first_name ||
+		user.last_name !== dbuser.last_name ||
+		user.username !== dbuser.username ||
+		user.is_premium !== dbuser.is_premium
+	) {
+		await database.updateUser(user!, {
+			first_name: user.first_name,
+			last_name: user.last_name ?? null,
+			username: user.username ?? null,
+			is_premium: user.is_premium,
+		});
+
+		return next();
+	}
+
+	return next();
+};
