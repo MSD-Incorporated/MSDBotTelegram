b7db31d1eb34eea7a30a958e7a23d5d0d7a84dce
MasedMSD <68379695+MasedMSD@users.noreply.github.com>
2025-04-27 15:29:00 +0500

    start composer

--- src/structures/client.ts
+++ src/structures/client.ts
@@ -1,6 +1,7 @@
 import { parseMode } from "@grammyjs/parse-mode";
 import { Bot } from "grammy";
 
+import { startComposer } from "../elements";
 import { autoQuote, autoUserCaching, createContextConstructor, type Context } from "../utils";
 import { Database } from "./database";
 
@@ -16,6 +17,8 @@ client.use(autoQuote());
 client.api.config.use(parseMode("HTML"));
 
 client.use(async (ctx, next) => autoUserCaching(ctx, database, next));
+
+client.use(startComposer);
 
 process.once("SIGINT", () => client.stop());
 process.once("SIGTERM", () => client.stop());
