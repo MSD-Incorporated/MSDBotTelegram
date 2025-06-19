df6363a4db84300644dbaca32a04cffefa6dccf1
MasedMSD <68379695+MasedMSD@users.noreply.github.com>
2025-06-19 06:02:19 +0500

    update dockerfile & userinfo banner

--- Dockerfile
+++ Dockerfile
@@ -10,29 +10,31 @@ COPY package.json bun.lock ./
 COPY --from=bun_image /usr/local/bin/bun /usr/local/bin/
 
 ENV NODE_ENV=production
-RUN --mount=type=cache,target=/etc/apk/cache apk add --update-cache build-base cairo-dev pango-dev librsvg-dev
+RUN --mount=type=cache,target=/etc/apk/cache apk add --update-cache pango-dev librsvg-dev
 RUN --mount=type=cache,target=/root/.npm bun --frozen-lockfile install
 
 RUN bun run typesafe-i18n --no-watch
+RUN bun run build
 
 # App
 FROM base_image AS app
 WORKDIR /app
 
 COPY --from=bun_image /usr/local/bin/bun /usr/local/bin/
-COPY ./src ./src
+COPY --from=build ./dist ./
+COPY ./src/resources ./src/resources
 COPY package.json bun.lock ./
-COPY node_modules /app/node_modules
 
 ENV NODE_ENV=production
-RUN --mount=type=cache,target=/etc/apk/cache apk add --update-cache cairo-dev pango-dev librsvg-dev fontconfig
-RUN --mount=type=cache,target=/root/.npm bun --frozen-lockfile --production install
+RUN --mount=type=cache,target=/etc/apk/cache apk add --update-cache pango-dev fontconfig
+RUN --mount=type=cache,target=/root/.npm bun --frozen-lockfile --production -f install
 
 RUN wget https://github.com/sahibjotsaggu/San-Francisco-Pro-Fonts/raw/refs/heads/master/SF-Pro-Display-Bold.otf \
 	&& mkdir -p /usr/share/fonts/custom \
 	&& cp /app/SF-Pro-Display-Bold.otf /usr/share/fonts/custom/ \ 
-	&& fc-cache -f -v
+	&& fc-cache -f -v \
+	&& rm -rf /app/SF-Pro-Display-Bold.otf
 
 RUN apk del fontconfig
 
-CMD [ "bun", "run", "src/index.ts" ]
+CMD [ "bun", "run", "index.js" ]
--- src/elements/ui.ts
+++ src/elements/ui.ts
@@ -1,4 +1,4 @@
-import { createCanvas, loadImage } from "canvas";
+import { createCanvas, loadImage } from "@napi-rs/canvas";
 import { desc } from "drizzle-orm";
 import { Composer, InputFile } from "grammy";
 import { resolve } from "path";
@@ -14,6 +14,9 @@ const background = (await loadImage(
 )) as unknown as CanvasImageSource;
 const background_white = (await loadImage(
 	resolve(process.cwd(), "src", "resources", "background-white.png")
+)) as unknown as CanvasImageSource;
+const avatarCleared = (await loadImage(
+	resolve(process.cwd(), "src", "resources", "avatar-cleared-white.svg")
 )) as unknown as CanvasImageSource;
 const plus_icon = (await loadImage(
 	resolve(process.cwd(), "src", "resources", "plus-icon.svg")
@@ -137,6 +140,43 @@ export const drawAvatar = (
 	return ctx.restore();
 };
 
+export const drawFadedRoundedAvatar = (
+	ctx: CanvasRenderingContext2D,
+	avatar: CanvasImageSource,
+	boxX: number,
+	boxY: number
+) => {
+	const w = 540;
+	const h = 510;
+	const r = Math.min(w, h) / 24;
+
+	const tempCanvas = createCanvas(w, h);
+	const tempCtx = tempCanvas.getContext("2d") as unknown as CanvasRenderingContext2D;
+
+	tempCtx.beginPath();
+	tempCtx.moveTo(r, 0);
+	tempCtx.arcTo(w, 0, w, r, r);
+	tempCtx.arcTo(w, h, w - r, h, r);
+	tempCtx.arcTo(0, h, 0, h - r, r);
+	tempCtx.arcTo(0, 0, r, 0, r);
+	tempCtx.closePath();
+	tempCtx.clip();
+
+	tempCtx.drawImage(avatar, 0, 0, w, h);
+
+	const gradient = tempCtx.createLinearGradient(0, 0, 0, h);
+	gradient.addColorStop(0, "rgba(0, 0, 0, 0.08)");
+	gradient.addColorStop(1, "rgba(0, 0, 0, 0)");
+
+	tempCtx.globalCompositeOperation = "destination-in";
+	tempCtx.fillStyle = gradient;
+
+	tempCtx.fillRect(0, 0, w, h + 1);
+
+	ctx.drawImage(tempCanvas as unknown as CanvasImageSource, boxX, boxY);
+	ctx.drawImage(avatarCleared, 910, 220);
+};
+
 userinfoComposer.command("userinfo", async ctx => {
 	const name = ctx.from?.first_name + (ctx.from?.last_name ? ` ${ctx.from?.last_name}` : "");
 
@@ -149,19 +189,6 @@ userinfoComposer.command("userinfo", async ctx => {
 	const canvas_context = canvas.getContext("2d") as unknown as CanvasRenderingContext2D;
 
 	canvas_context.drawImage(ctx.from!.id !== 946070039 ? background : background_white, 0, 0);
-
-	const user_photo = (await ctx.api.getUserProfilePhotos(ctx.from?.id!)).photos;
-	if (user_photo?.length) {
-		const file = await ctx.api.getFile(user_photo[0]![0]!.file_id);
-
-		const bun_file =
-			process.env.LOCAL_API === undefined
-				? await fetch(`https://api.telegram.org/file/bot${process.env.TOKEN}/${file.file_path!}`)
-				: Bun.file(file.file_path!);
-		const buffer = Buffer.from(await bun_file.arrayBuffer());
-		const avatar = (await loadImage(buffer)) as unknown as CanvasImageSource;
-		drawAvatar(canvas_context, avatar, 924, 234);
-	}
 
 	const customTitle = ((await ctx.getChatMember(ctx.from!.id)) as ChatMember & { custom_title?: string })
 		?.custom_title;
@@ -177,6 +204,21 @@ userinfoComposer.command("userinfo", async ctx => {
 	drawDickSize(canvas_context, dickSize.toString() + " см", 351, 195);
 	drawLevelTitle(canvas_context, "Fucking Slave", 258, 274);
 
+	const user_photo = (await ctx.api.getUserProfilePhotos(ctx.from?.id!)).photos;
+	if (user_photo?.length) {
+		const file = await ctx.api.getFile(user_photo[0]![0]!.file_id);
+
+		const bun_file =
+			process.env.LOCAL_API === undefined
+				? await fetch(`https://api.telegram.org/file/bot${process.env.TOKEN}/${file.file_path!}`)
+				: Bun.file(file.file_path!);
+		const buffer = Buffer.from(await bun_file.arrayBuffer());
+		const avatar = (await loadImage(buffer)) as unknown as CanvasImageSource;
+
+		if (ctx.from!.id === 946070039) drawFadedRoundedAvatar(canvas_context, avatar, 740, 130);
+		drawAvatar(canvas_context, avatar, 924, 234);
+	}
+
 	dick.history.forEach(({ difference }, index) => {
 		const diff = difference.toString().replace("-", "");
 		const x: number = 149 as const;
@@ -187,7 +229,7 @@ userinfoComposer.command("userinfo", async ctx => {
 		return drawDickHistory(canvas_context, diff, x, y, zero_icon, "zero");
 	});
 
-	const attachment = new InputFile(canvas.toBuffer(), "avatar.png");
+	const attachment = new InputFile(canvas.toBuffer("image/png"), "avatar.png");
 	return ctx.replyWithPhoto(attachment, {
 		caption: [
 			`👤 <b>Информация о:</b> ${name} [<code>${ctx.from?.id}</code>]\n`,
