498cccef4af65d1da32ee479acb45c67c14903ab
MasedMSD <68379695+MasedMSD@users.noreply.github.com>
2025-10-19 01:29:01 +0500

    feat: 🧹 Cleaning

--- src/drizzle/index.ts
+++
@@ -1,8 +0,0 @@
-export * from "./msdbot/dick";
-export * from "./msdbot/referrals";
-export * from "./msdbot/user";
-
-// export * from "./chat";
-export * from "./user";
-
-export * from "./relations";
--- src/elements/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./dick";
-export * from "./extra";
-export * from "./msdIncorporated";
-export * from "./start";
-export * from "./ui";
