ba8046107d522ef3db13e9b4b825b139d4507c6c
MasedMSD <68379695+MasedMSD@users.noreply.github.com>
2025-11-13 19:49:19 +0500

    feat: Add utils package and use onStart function
    Refactor bot client to use the onStart function from the new utils
    package. Also add drop_pending_updates.

--- apps/bot/src/structures/client.ts
+++ apps/bot/src/structures/client.ts
@@ -1,13 +1,7 @@
 import { env } from "@msdbot/env";
+import { onStart } from "@msdbot/utils";
 import { Bot } from "grammy";
-import type { UserFromGetMe } from "grammy/types";
 
 export const client = new Bot(env.BOT_TOKEN, {});
 
-export const onStart = ({ id, username, first_name }: UserFromGetMe) =>
-	console.log(`${first_name} | @${username} [${id}] started!`);
-
-client.start({
-	onStart,
-	allowed_updates: ["message", "callback_query"],
-});
+client.start({ onStart, allowed_updates: ["message", "callback_query"], drop_pending_updates: true });
