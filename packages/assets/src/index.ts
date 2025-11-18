22028661fd26fb9d1e897bfb4c925a2009ad03ff
MasedMSD <68379695+MasedMSD@users.noreply.github.com>
2025-11-19 00:08:44 +0500

    little rename assets

--- packages/assets/src/index.ts
+++ packages/assets/src/index.ts
@@ -1,5 +1,5 @@
-import im_here_image from "./assets/im_here.png" with { type: "file" };
-import referral_image from "./assets/referral.png" with { type: "file" };
+import im_here_image from "./assets/im_here_banner.png" with { type: "file" };
+import referral_image from "./assets/referral_banner.png" with { type: "file" };
 
 export const im_here_banner = await Bun.file(im_here_image).bytes();
 export const referral_banner = await Bun.file(referral_image).bytes();
