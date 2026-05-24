d7c339e98c111d93928e96c986d76f812af1802b
MasedMSD <68379695+MasedMSD@users.noreply.github.com>
2026-05-24 22:21:05 +0500

    Bump Prettier to 3.8.3 and simplify DB config

--- packages/database/drizzle.config.ts
+++ packages/database/drizzle.config.ts
@@ -1,19 +1,11 @@
 import env from "@msdbot/env";
 import { defineConfig } from "drizzle-kit";
 
-const { HOST, PORT, USER, PASSWORD, DATABASE, URL,} = env.DATABASE;
+const { HOST, PORT, USER, PASSWORD, DATABASE, URL } = env.DATABASE;
 
 export default defineConfig({
 	schema: "./src/drizzle/*",
 	dialect: "postgresql",
 	out: "./drizzle",
-	dbCredentials: {
-		host: HOST ?? "localhost",
-		port: PORT ?? 5432,
-		user: USER,
-		password: PASSWORD,
-		database: DATABASE!,
-		url: URL ?? undefined,
-		ssl: false,
-	},
+	dbCredentials: { host: HOST, port: PORT, user: USER, password: PASSWORD, database: DATABASE, url: URL, ssl: false },
 });
