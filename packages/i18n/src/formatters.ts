e2fd3333a322f7a4a32777a48a9dbc930666b54c
MasedMSD <68379695+MasedMSD@users.noreply.github.com>
2026-07-27 19:44:16 +0000

    Apply formatting changes

--- packages/i18n/src/formatters.ts
+++ packages/i18n/src/formatters.ts
@@ -167,9 +167,9 @@ export const dateFormatter: Intl.DateTimeFormat = new Intl.DateTimeFormat("ru", 
 
 export const initFormatters: FormattersInitializer<Locales, Formatters> = (_locale: Locales) => {
 	const formatters: Formatters = {
-        // 'value' will receive the actual number timestamp at runtime
-        formatDate: (value: number) => dateFormatter.format(new Date(value * 1000)),
-    };
+		// 'value' will receive the actual number timestamp at runtime
+		formatDate: (value: number) => dateFormatter.format(new Date(value * 1000)),
+	};
 
 	return formatters;
 };
