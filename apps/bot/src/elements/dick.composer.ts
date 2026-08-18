bdcf8ef53786c48718d3c8ce6fb134cdc04e3a56
MasedMSD <68379695+MasedMSD@users.noreply.github.com>
2026-08-19 03:44:33 +0500

    testing cache

--- apps/bot/src/elements/dick.composer.ts
+++ apps/bot/src/elements/dick.composer.ts
@@ -20,7 +20,7 @@ import { randomInt } from "node:crypto";
 import { dateFormatter, formatTime, isSubscriber, keyboardBuilder, normalizeName, type Context } from "../utils";
 
 export const dickComposer = new Composer<Context>();
-export const timeout: number = 6 * 60 * 60 * 1000;
+export const timeout = 6 * 60 * 60 * 1000;
 export const referral_timeout: number = 24 * 60 * 60 * 1000;
 
 const TYPES: ("dick" | "dice" | "referral" | "transfer")[] = ["dick", "dice", "referral", "transfer"] as const;
